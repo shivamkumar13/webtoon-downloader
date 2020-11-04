8b296d73457ca62e6584b9831cec5ad7619da2fe
Shivam <shivamkumar001@gmail.com>
2020-11-04 21:23:21 +0530

    Modified 1.0

--- webtoon-downloader.sh
+++ webtoon-downloader.sh
@@ -4,36 +4,57 @@
 # chmod +X <filename.sh>
 
 #download from manhwa
-if [ $# -ne 3 ]; then
-	echo "<Parent URL> <Name of Comic> <Number of Chapters>"
-	echo "All three space separated"
-	echo "eg-> https://toonily.com/webtoon/ the-boy-who-slept 3"
-	exit -1
-fi
-
-echo $1 $2 $3
 
 curl=$(which curl)
 outfile="url.txt"
 regex="lazyload effect-fade"
-manga_id="manga_5bf3793e73db1"
+manga_id="manga_5ef28dfd223b7"
 url=$1
 manga_name=$2
 last_chapter=$3
 chapter_number=0
 chapter_array={01..$end_chapter}
 
+if [ $# -ne 3 ]; then
+	echo ""
+	echo "<Parent URL> <Name of Comic> <Number of Chapters>"
+	echo "OR"
+	echo "bash webtoon-downloader.sh <Parent URL> <Name of Comic> <Number of Chapters>"
+	echo ""
+	echo "All three space separated"
+	echo "eg-> https://toonily.com/webtoon/ the-boy-who-slept 3"
+	echo ""
+	echo "Please check manga_id and effect class name"
+	echo -e "Current manga_id \t:$manga_id"
+	echo -e "Current effect class \t:$regex"
+	echo ""
+	echo "check urls/ manga id by typing"
+	echo "curl -s -o page_source.txt <chapter_url>"
+	echo "grep -h -e \"manga_\" page_source.txt | sed -n -e 's/\" class.*//p'| sed 's/^.*\(http.*jpg\).*$/\1/' > url_list.txt"
+	echo ""
+	echo "check url_list, it should contain url in form of"
+	echo "https://toonily.com/wp-content/uploads/WP-manga/data/manga_5ef28dfd223b7/c86ccd727570a39e3bc673de1a6e2dc4/002.jpg"
+
+	exit -1
+fi
+
+echo $1 $2 $3
+
+
+
 #--------------------------------------------------------------------------------------------------------------
 
 function find_url_list(){
+	# curl chapter url source code
 	$curl -s -o  $outfile $chapter_url
-	grep -h -e "manga_" $outfile | sed -n -e 's/" class.*//p'| sed -e 's/\t//g' > temp.txt
+	# extract url
+	grep -h -e "manga_" $outfile | sed -n -e 's/" class.*//p'| sed 's/^.*\(http.*jpg\).*$/\1/' > temp.txt
 	cp temp.txt $outfile
 	rm temp.txt
 }
 
 function download_files(){
-	wget -qNi $outfile
+	wget -qNi $outfile --show-progress --progress=bar:force 2>&1
 }
 #--------------------------------------------------------------------------------------------------------------
 function download_chapter(){
@@ -49,7 +70,16 @@ function doing_chapters(){
 		printf "\n\t%s:\t%s" "$chapter_name" "$chapter_url"
 		find_url_list
 		download_chapter # Download all files in url.txt
+
+		
+		no_of_url_in_outfile="$(cat $outfile | wc -l)"
+		echo ""
+		printf " \t URL:%s" "$no_of_url_in_outfile"
 		rm $outfile
+		printf " Files:%s" "$(ls | wc -l)"
+		printf " Completed:%s" "$chapter_name"
+		echo ""
+		echo "."
 		cd ..
 }
 function doing_last_chapter(){
@@ -121,6 +151,14 @@ function renaming(){
 
 
 function chapters_renaming(){
+	###########################
+#         RENAMING        #
+###########################
+echo "########### RENAMING ###############"
+echo "This is done to have a Bookmark of chapters from Images"
+echo "if file 01.jpg OR 001.jpg exist chaged to 01-Chapter-<number>.jpg OR 001-Chapter-<number>.jpg"
+chapters_renaming
+
 	c=1
 	cd $manga_name 
 	for (( c=1; c<=$last_chapter; c++ ))
@@ -130,10 +168,3 @@ function chapters_renaming(){
 	cd ..
 }
 
-###########################
-#         RENAMING        #
-###########################
-echo "########### RENAMING ###############"
-echo "This is done to have a Bookmark of chapters from Images"
-echo "if file 01.jpg OR 001.jpg exist chaged to 01-Chapter-<number>.jpg OR 001-Chapter-<number>.jpg"
-chapters_renaming
